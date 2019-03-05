3171b8e21648ebf344c1dbaa125f3c9f2baf25b5
Mahram Gurbanzade <mgurbanzade@yahoo.com>
2019-03-05 11:55:55 +0400

    Add .ini support and release new version

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,20 +6,25 @@ const findFixture = name => path.join(__dirname, '__fixtures__', name);
 const expected = fs.readFileSync(findFixture('expected'), 'utf-8');
 const sampleTitle = 'Calculate difference between two';
 
-test(`${sampleTitle} JSON files`, () => {
-  const file1 = findFixture('before.json');
-  const file2 = findFixture('after.json');
-  expect(gendiff(file1, file2)).toBe(expected);
-});
-
-test(`${sampleTitle} YAML files`, () => {
-  const file1 = findFixture('before.yml');
-  const file2 = findFixture('after.yml');
-  expect(gendiff(file1, file2)).toBe(expected);
-});
+test.each(['.json', '.yml', '.ini'])(
+  `${sampleTitle} %s files`, i => expect(gendiff(findFixture(`before${i}`), findFixture(`after${i}`))).toBe(expected),
+);
 
 test(`${sampleTitle} files with different extensions`, () => {
   const file1 = findFixture('before.yml');
   const file2 = findFixture('after.json');
   expect(gendiff(file1, file2)).toBe(expected);
 });
+
+const crossExt = [
+  ['.json', '.yml'],
+  ['.yml', '.json'],
+  ['.json', '.ini'],
+  ['.ini', '.json'],
+  ['.yml', '.ini'],
+  ['.ini', '.yml'],
+];
+
+test.each(crossExt)(
+  `${sampleTitle} files with extensions of %s and %s`, (ex1, ex2) => expect(gendiff(findFixture(`before${ex1}`), findFixture(`after${ex2}`))).toBe(expected),
+);
--- src/parsers.js
+++ src/parsers.js
@@ -1,16 +1,17 @@
 import fs from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
+import ini from 'ini';
 
-const validFormats = new Set(['.json', '.yml']);
+const validFormats = new Set(['.json', '.yml', '.ini']);
 const parsers = {
-  '.yml': yaml.safeLoad,
-  '.json': JSON.parse,
+  '.yml': src => yaml.safeLoad(fs.readFileSync(src)),
+  '.json': src => JSON.parse(fs.readFileSync(src)),
+  '.ini': src => ini.parse(fs.readFileSync(src, 'utf-8')),
 };
 
 export default (src) => {
   const isValidFile = validFormats.has(path.extname(src));
   if (!isValidFile) return null;
-  const parser = parsers[path.extname(src)];
-  return parser(fs.readFileSync(src).toString());
+  return parsers[path.extname(src)](src);
 };
