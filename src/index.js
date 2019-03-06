384176d65c68e2d3069e20b3efc479401f15224b
Mahram Gurbanzade <mgurbanzade@yahoo.com>
2019-03-06 10:11:51 +0400

    Get rid of mutation

--- src/index.js
+++ src/index.js
@@ -16,17 +16,23 @@ export default (...args) => {
   const [filePath1, filePath2] = args.map(filePath => parse(filePath));
   const keys = _.union(_.keys(filePath1), _.keys(filePath2));
   const diff = keys.reduce((acc, key) => {
-    const tempAcc = [];
     if (filePath1[key] === filePath2[key]) {
       return [...acc, stringify(' ', key, filePath1[key])];
     }
-    if (_.has(filePath2, key)) {
-      tempAcc.push(stringify('+', key, filePath2[key]));
+
+    if (_.has(filePath2, key) && _.has(filePath1, key)) {
+      return [
+        ...acc,
+        stringify('+', key, filePath2[key]),
+        stringify('-', key, filePath1[key]),
+      ];
     }
-    if (_.has(filePath1, key)) {
-      tempAcc.push(stringify('-', key, filePath1[key]));
+
+    if (_.has(filePath2, key) && !_.has(filePath1, key)) {
+      return [...acc, stringify('+', key, filePath2[key])];
     }
-    return [...acc, ...tempAcc];
+
+    return [...acc, stringify('-', key, filePath1[key])];
   }, []);
 
   return _.flattenDeep(['{', diff, '}\n']).join('\n');
