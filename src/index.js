e771461831bcf6abdb74d1493c8d414cb029e77c
Mahram Gurbanzade <mgurbanzade@yahoo.com>
2019-03-06 14:58:16 +0400

    Fix naming errors

--- src/index.js
+++ src/index.js
@@ -1,38 +1,40 @@
-import fs from 'fs';
 import _ from 'lodash';
+import fs from 'fs';
 import path from 'path';
 import getParser from './parsers';
 
 const stringify = (sign, key, val) => `  ${sign} ${key}: ${val}`;
 const getExtension = filePath => path.extname(filePath);
 
-const parse = (filePath) => {
-  const ext = getExtension(filePath);
-  const parseFile = getParser(ext);
-  return parseFile(fs.readFileSync(filePath, 'utf-8'));
+const readDataByPath = (pathToData) => {
+  const ext = getExtension(pathToData);
+  const parseData = getParser(ext);
+  return parseData(fs.readFileSync(pathToData, 'utf-8'));
 };
 
-export default (...args) => {
-  const [filePath1, filePath2] = args.map(filePath => parse(filePath));
-  const keys = _.union(_.keys(filePath1), _.keys(filePath2));
-  const diff = keys.reduce((acc, key) => {
-    if (filePath1[key] === filePath2[key]) {
-      return [...acc, stringify(' ', key, filePath1[key])];
+export default (firstConfigPath, secondConfigPath) => {
+  const firstDataObj = readDataByPath(firstConfigPath);
+  const secondDataObj = readDataByPath(secondConfigPath);
+  const dataObjKeys = _.union(_.keys(firstDataObj), _.keys(secondDataObj));
+
+  const diff = dataObjKeys.reduce((acc, key) => {
+    if (firstDataObj[key] === secondDataObj[key]) {
+      return [...acc, stringify(' ', key, firstDataObj[key])];
     }
 
-    if (_.has(filePath2, key) && _.has(filePath1, key)) {
+    if (_.has(secondDataObj, key) && _.has(firstDataObj, key)) {
       return [
         ...acc,
-        stringify('+', key, filePath2[key]),
-        stringify('-', key, filePath1[key]),
+        stringify('+', key, secondDataObj[key]),
+        stringify('-', key, firstDataObj[key]),
       ];
     }
 
-    if (_.has(filePath2, key) && !_.has(filePath1, key)) {
-      return [...acc, stringify('+', key, filePath2[key])];
+    if (_.has(secondDataObj, key) && !_.has(firstDataObj, key)) {
+      return [...acc, stringify('+', key, secondDataObj[key])];
     }
 
-    return [...acc, stringify('-', key, filePath1[key])];
+    return [...acc, stringify('-', key, firstDataObj[key])];
   }, []);
 
   return _.flattenDeep(['{', diff, '}\n']).join('\n');
